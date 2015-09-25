95d663b62f4bc64dfbfbf70d4ac00aa843a6ceb8
Shin'ya Ueoka <ibenza@i-beam.org>
2015-09-25 11:08:39 +0900

    Remove include vitiation

--- spec/vagrant_spec.rb
+++ spec/vagrant_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 require 'rails_app_helper'
 
 RSpec.describe 'Kerberos Authentication Server' do
-  include Krb5Auth
   describe 'abc' do
     it 'passes krb authentication' do
-      expect(Krb5.new.get_init_creds_password('dog@TG2KRB5.COM', 'bowwow')).to be true
-      expect(Krb5.new.get_init_creds_password('cat@TG2KRB5.COM', 'mewmew')).to be true
+      expect(Krb5Auth::Krb5.new.get_init_creds_password('dog@TG2KRB5.COM', 'bowwow')).to be true
+      expect(Krb5Auth::Krb5.new.get_init_creds_password('cat@TG2KRB5.COM', 'mewmew')).to be true
     end
 
     it 'failed exception' do
-      expect{Krb5.new.get_init_creds_password('oinkoink@TG2KRB5.COM', 'mewmew')}.to raise_error(Krb5::Exception)
+      expect{Krb5Auth::Krb5.new.get_init_creds_password('oinkoink@TG2KRB5.COM', 'mewmew')}.to raise_error(Krb5Auth::Krb5::Exception)
     end
   end
 end
