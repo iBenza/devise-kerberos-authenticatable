e8cd37ebe4bd14451bc44f01559645b23f8ece13
Shin'ya Ueoka <ibenza@i-beam.org>
2015-09-25 11:09:56 +0900

    Use let for kerb5 instance

--- spec/vagrant_spec.rb
+++ spec/vagrant_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 require 'rails_app_helper'
 
 RSpec.describe 'Kerberos Authentication Server' do
+  let (:krb5) { Krb5Auth::Krb5.new }
+
   describe 'abc' do
     it 'passes krb authentication' do
-      expect(Krb5Auth::Krb5.new.get_init_creds_password('dog@TG2KRB5.COM', 'bowwow')).to be true
-      expect(Krb5Auth::Krb5.new.get_init_creds_password('cat@TG2KRB5.COM', 'mewmew')).to be true
+      expect(krb5.get_init_creds_password('dog@TG2KRB5.COM', 'bowwow')).to be true
+      expect(krb5.get_init_creds_password('cat@TG2KRB5.COM', 'mewmew')).to be true
     end
 
     it 'failed exception' do
-      expect{Krb5Auth::Krb5.new.get_init_creds_password('oinkoink@TG2KRB5.COM', 'mewmew')}.to raise_error(Krb5Auth::Krb5::Exception)
+      expect{krb5.get_init_creds_password('oinkoink@TG2KRB5.COM', 'mewmew')}.to raise_error(Krb5Auth::Krb5::Exception)
     end
   end
 end
